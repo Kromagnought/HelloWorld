e85cc7365f6e6d5de6587920d3cd6dded59427f2
Kevin Murphy <kevin.murphy@kpmg.co.uk>
2017-06-02 16:36:59 +0100

    3rd commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World - Git Test!");
+            Console.WriteLine("I just added another line to Test Git!");
+
+            Console.ReadKey();
+        }
+    }
+}
